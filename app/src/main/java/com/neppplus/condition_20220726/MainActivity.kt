5fb4499b06a672aef66667f9297a6f51ee9c42e9
김민철 <kmc901206@gmail.com>
2022-07-26 15:26:54 +0900

    논리 연산자 활용(로직 생각)

--- app/src/main/java/com/neppplus/condition_20220726/MainActivity.kt
+++ app/src/main/java/com/neppplus/condition_20220726/MainActivity.kt
@@ -2,6 +2,8 @@ package com.neppplus.condition_20220726
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,31 +11,53 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn.setOnClickListener {
-//            val age = inputEdt.text.toString().toInt()
-//
-//            if (age < 10) {
-////                if문이 true일 경우 실행
-//                answerTxt.text = "유아입니다."
-//            }
-//            else if (age < 20) {
-////                else if 문이 true일 경우 실행
-//                answerTxt.text = "청소년입니다."
-//            }
-//            else {
-////                if문이 false일 경우 실행
-//                answerTxt.text = "성인입니다."
-//            }
-            val inputData : String = inputEdt.text.toString()
+        variationTestBtn.setOnClickListener {
+            val myBirthYear = 1990
+            val myAge = 2022 - myBirthYear + 1
 
-            if (inputData == "김민철") {
-                answerTxt.text = "김민철이 맞습니다."
+            if (myAge < 10) {
+                Log.d("나이 검사", "유아입니다.")
+            }
+            else if (myAge < 20) {
+                Log.d("나이 검사", "청소년입니다.")
             }
             else {
-//                조건식 : inputData != "김민철"
-                answerTxt.text = "김민철이 아닙니다."
+                Log.d("나이 검사", "성인입니다.")
+            }
+        }
+
+
+        conditionBtn2.setOnClickListener {
+//        장기 근속 1. 연봉, 2. 야근 유무, 거리
+            val income = 3800
+            val dayNight = true
+            val distance = 4
+
+//        1. 연봉이 4000이상이거나 or 거리가 5분 이내거나 (OR 연산자 활용)
+            if (income >= 4000 || distance <= 5) {
+                Log.d("입사 여부", "취업 OK")
+            }
+            else {
+                Log.d("입사 여부", "다른 회사 지원")
             }
 
+//        2. 연봉이 3000이상 이거나 or 야근이 없거나(Nor 연산자 OR 연산자)
+            if (income >= 3000 || !dayNight) {
+                Log.d("입사 여부", "취업 OK")
+            }
+            else {
+                Log.d("입사 여부", "다른 회사 지원")
+            }
+
+//        3. 야근이 없어야하고 and 거리가 10분 이내(AND 연산자 활용)
+            if (!dayNight && distance < 10) {
+                Log.d("입사 여부", "취업 OK")
+            }
+            else {
+                Log.d("입사 여부", "다른 회사 지원")
+            }
         }
+
+
     }
 }
